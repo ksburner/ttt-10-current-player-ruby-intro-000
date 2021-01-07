5a6201dd29ba206faf6ceaf7a11e0da2ae7e0d2f
Kendra Swanson <kcs7@rice.edu>
2021-01-07 16:08:07 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
   turns = 0
   board.each do |space|
-    !(space == " " || space == "") ? turns += 1
+    !(space == " " || space == "") ? turns += 1 : turns += 0
   end
   return turns
 end
